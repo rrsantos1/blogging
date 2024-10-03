95bbc487703688722afd7b0dd45e8ad021890ab3
Robson <rrsantos1@hotmail.com>
2024-10-03 11:18:51 -0300

    alteração do workflow, do docker-compose e inclusão do start.sh para aguardar o banco ficar pronto para receber conexões

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Aguardando o banco de dados estar pronto
-echo "Aguardando o banco de dados estar pronto..."
 until nc -z db 5432; do
+  echo "Aguardando o banco de dados estar pronto..."
   sleep 1
 done
 
