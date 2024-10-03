38105a4d2f39af8787686297e33e22c264c101c1
Robson <rrsantos1@hotmail.com>
2024-10-03 14:38:22 -0300

    adequação do modo production no .env

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Aguardando o banco de dados estar pronto
-until nc -z blogging-db-1 5432; do
+until nc -z db 5432; do
   echo "Aguardando o banco de dados estar pronto..."
   sleep 1
 done
